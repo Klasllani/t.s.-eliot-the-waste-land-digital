0cec5035b72237adb81b94671663f85017732cca
Klaybis Asllani <klaybisa@gmail.com>
2025-03-20 15:30:43 -0500

    Update LuceneIndexer.java

--- LuceneIndexer.java
+++ LuceneIndexer.java
@@ -1,32 +1,37 @@
-package com.example;
-
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.RAMDirectory;
+import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import java.nio.file.Paths;
 
 public class LuceneIndexer {
     public static void main(String[] args) {
-        try (Directory directory = new RAMDirectory();
-             StandardAnalyzer analyzer = new StandardAnalyzer();
-             IndexWriter writer = new IndexWriter(directory, new IndexWriterConfig(analyzer))) {
+        try {
+            // Create a directory to store the index on disk
+            Directory directory = FSDirectory.open(Paths.get("lucene-index"));
+            StandardAnalyzer analyzer = new StandardAnalyzer();
+            IndexWriterConfig config = new IndexWriterConfig(analyzer);
+            IndexWriter writer = new IndexWriter(directory, config);
             
             // Parse the XML file
-            String content = XMLParser.parseXML("The-Waste-Land.xml");
+            String xmlFile = args.length > 0 ? args[0] : "The-Waste-Land.xml";
+            String content = XMLParser.parseXML(xmlFile);
             
             // Create a Lucene document and add fields
             Document doc = new Document();
             doc.add(new TextField("content", content, Field.Store.YES));
             writer.addDocument(doc);
             
-            // Commit changes
+            // Commit changes and close
             writer.commit();
+            writer.close();
+            directory.close();
             
-            System.out.println("Indexing completed.");
+            System.out.println("Indexing completed. Index stored in 'lucene-index' directory.");
         } catch (Exception e) {
             e.printStackTrace();
         }
