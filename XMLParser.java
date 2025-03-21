cdf1c98792d6748a64d16dcac4953de266ecd657
Klaybis Asllani <klaybisa@gmail.com>
2025-03-20 19:57:04 -0500

    Update XMLParser.java

--- XMLParser.java
+++ XMLParser.java
@@ -1,48 +1,73 @@
-import java.io.File;
-import java.nio.file.Files;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import java.io.File;
 
 public class XMLParser {
-    
-    public static String parseXML(String filename) {
-        StringBuilder textContent = new StringBuilder();
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Usage: java XMLParser <xml-file>");
+            System.exit(1);
+        }
+        
+        String xmlFile = args[0];
         
         try {
-            // Check if file exists
-            File xmlFile = new File(filename);
-            if (!xmlFile.exists()) {
-                System.err.println("File not found: " + filename);
-                return "";
-            }
-            
-            // Create DocumentBuilder
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
             DocumentBuilder builder = factory.newDocumentBuilder();
-            Document doc = builder.parse(xmlFile);
-            doc.getDocumentElement().normalize();
+            Document document = builder.parse(new File(xmlFile));
             
-            // Extract text from all <l> elements (lines)
-            NodeList lineNodes = doc.getElementsByTagName("l");
-            for (int i = 0; i < lineNodes.getLength(); i++) {
-                Node node = lineNodes.item(i);
-                if (node.getNodeType() == Node.ELEMENT_NODE) {
-                    Element element = (Element) node;
-                    textContent.append(element.getTextContent()).append("\n");
+            document.getDocumentElement().normalize();
+            
+            // Print root element
+            System.out.println("Root element: " + document.getDocumentElement().getNodeName());
+            
+            // Get all div elements
+            NodeList divList = document.getElementsByTagName("div");
+            System.out.println("Number of sections: " + divList.getLength());
+            
+            // Process each div (section)
+            for (int i = 0; i < divList.getLength(); i++) {
+                Element div = (Element) divList.item(i);
+                String type = div.getAttribute("type");
+                
+                if (type.equals("section")) {
+                    // Get section title
+                    NodeList headList = div.getElementsByTagName("head");
+                    String title = "Untitled";
+                    if (headList.getLength() > 0) {
+                        title = headList.item(0).getTextContent();
+                    }
+                    
+                    System.out.println("\nSection " + (i+1) + ": " + title);
+                    
+                    // Get stanzas
+                    NodeList lgList = div.getElementsByTagName("lg");
+                    System.out.println("Number of stanzas: " + lgList.getLength());
+                    
+                    // Process each stanza
+                    for (int j = 0; j < lgList.getLength(); j++) {
+                        Element lg = (Element) lgList.item(j);
+                        
+                        System.out.println("\n  Stanza " + (j+1) + ":");
+                        
+                        // Get lines
+                        NodeList lList = lg.getElementsByTagName("l");
+                        
+                        // Process each line
+                        for (int k = 0; k < lList.getLength(); k++) {
+                            Element l = (Element) lList.item(k);
+                            String lineText = l.getTextContent();
+                            System.out.println("    Line " + (k+1) + ": " + lineText);
+                        }
+                    }
                 }
             }
             
-            System.out.println("Successfully parsed XML file: " + filename);
-            
         } catch (Exception e) {
-            System.err.println("Error parsing XML: " + e.getMessage());
             e.printStackTrace();
         }
-        
-        return textContent.toString();
     }
 }
