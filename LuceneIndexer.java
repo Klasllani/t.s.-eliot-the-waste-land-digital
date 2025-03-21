93044f6d1ec2ed9267f9499c55c2271c44656f16
Klaybis Asllani <klaybisa@gmail.com>
2025-03-20 19:54:44 -0500

    Update LuceneIndexer.java

--- LuceneIndexer.java
+++ LuceneIndexer.java
@@ -1,39 +1,97 @@
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
 import java.nio.file.Paths;
 
 public class LuceneIndexer {
     public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Usage: java LuceneIndexer <xml-file>");
+            System.exit(1);
+        }
+        
+        String xmlFile = args[0];
+        
         try {
-            // Create a directory to store the index on disk
-            Directory directory = FSDirectory.open(Paths.get("lucene-index"));
+            // Parse XML
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            org.w3c.dom.Document xmlDoc = builder.parse(new File(xmlFile));
+            
+            // Setup Lucene
+            Directory indexDirectory = FSDirectory.open(Paths.get("index"));
             StandardAnalyzer analyzer = new StandardAnalyzer();
             IndexWriterConfig config = new IndexWriterConfig(analyzer);
-            IndexWriter writer = new IndexWriter(directory, config);
+            IndexWriter writer = new IndexWriter(indexDirectory, config);
             
-            // Parse the XML file
-            String xmlFile = args.length > 0 ? args[0] : "The-Waste-Land.xml";
-            String content = XMLParser.parseXML(xmlFile);
+            // Clear previous index
+            writer.deleteAll();
             
-            // Create a Lucene document and add fields
-            Document doc = new Document();
-            doc.add(new TextField("content", content, Field.Store.YES));
-            writer.addDocument(doc);
+            // Process poem by section, stanza
+            NodeList divElements = xmlDoc.getElementsByTagName("div");
+            int sectionCount = 0;
             
-            // Commit changes and close
-            writer.commit();
+            for (int i = 0; i < divElements.getLength(); i++) {
+                Element divElement = (Element) divElements.item(i);
+                String type = divElement.getAttribute("type");
+                
+                if (type.equals("section")) {
+                    sectionCount++;
+                    String sectionTitle = getSectionTitle(divElement);
+                    
+                    // Get stanzas (lg elements)
+                    NodeList lgElements = divElement.getElementsByTagName("lg");
+                    int stanzaCount = 0;
+                    
+                    for (int j = 0; j < lgElements.getLength(); j++) {
+                        Element lgElement = (Element) lgElements.item(j);
+                        stanzaCount++;
+                        
+                        // Get lines (l elements)
+                        NodeList lElements = lgElement.getElementsByTagName("l");
+                        StringBuilder stanzaText = new StringBuilder();
+                        
+                        for (int k = 0; k < lElements.getLength(); k++) {
+                            Element lElement = (Element) lElements.item(k);
+                            String lineText = lElement.getTextContent();
+                            stanzaText.append(lineText).append("\n");
+                        }
+                        
+                        // Create stanza document only
+                        Document stanzaDoc = new Document();
+                        stanzaDoc.add(new StringField("section", String.valueOf(sectionCount), Field.Store.YES));
+                        stanzaDoc.add(new StringField("sectionTitle", sectionTitle, Field.Store.YES));
+                        stanzaDoc.add(new StringField("stanza", String.valueOf(stanzaCount), Field.Store.YES));
+                        stanzaDoc.add(new TextField("content", stanzaText.toString(), Field.Store.YES));
+                        writer.addDocument(stanzaDoc);
+                    }
+                }
+            }
+            
             writer.close();
-            directory.close();
+            System.out.println("Indexing complete!");
             
-            System.out.println("Indexing completed. Index stored in 'lucene-index' directory.");
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+    
+    private static String getSectionTitle(Element sectionElement) {
+        NodeList headElements = sectionElement.getElementsByTagName("head");
+        if (headElements.getLength() > 0) {
+            return headElements.item(0).getTextContent();
+        }
+        return "Untitled Section";
     }
 }
