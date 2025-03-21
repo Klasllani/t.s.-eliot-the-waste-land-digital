ac02b9659c5f8e374c79da4ada0e32697848dfa2
Klaybis Asllani <klaybisa@gmail.com>
2025-03-20 19:55:28 -0500

    Update LuceneSearcher.java

--- LuceneSearcher.java
+++ LuceneSearcher.java
@@ -1,5 +1,7 @@
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
@@ -7,43 +9,50 @@ import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import java.nio.file.Paths;
 
 public class LuceneSearcher {
     public static void main(String[] args) {
-        if (args.length == 0) {
-            System.out.println("Usage: java LuceneSearcher <search term>");
-            return;
+        if (args.length != 1) {
+            System.out.println("Usage: java LuceneSearcher <search-term>");
+            System.exit(1);
         }
         
         String searchTerm = args[0];
         
         try {
-            // Open the directory where the index was stored
-            Directory directory = FSDirectory.open(Paths.get("lucene-index"));
-            DirectoryReader reader = DirectoryReader.open(directory);
+            // Open index
+            Directory indexDirectory = FSDirectory.open(Paths.get("index"));
+            IndexReader reader = DirectoryReader.open(indexDirectory);
             IndexSearcher searcher = new IndexSearcher(reader);
-            StandardAnalyzer analyzer = new StandardAnalyzer();
             
-            // Parse the query
+            // Parse query
+            StandardAnalyzer analyzer = new StandardAnalyzer();
             QueryParser parser = new QueryParser("content", analyzer);
             Query query = parser.parse(searchTerm);
             
-            // Execute the query
-            TopDocs results = searcher.search(query, 10);
-            System.out.println("Found " + results.scoreDocs.length + " matching documents for '" + searchTerm + "'");
+            // Search
+            TopDocs results = searcher.search(query, 100); // Get top 100 results
             
-            for (ScoreDoc scoreDoc : results.scoreDocs) {
+            System.out.println("Found " + results.totalHits.value + " stanzas for '" + searchTerm + "'");
+            
+            // Display results - stanzas only
+            for (int i = 0; i < results.scoreDocs.length; i++) {
+                ScoreDoc scoreDoc = results.scoreDocs[i];
                 Document doc = searcher.doc(scoreDoc.doc);
-                // Print the first 200 characters of the content to avoid overwhelming output
+                
+                String section = doc.get("section");
+                String sectionTitle = doc.get("sectionTitle");
+                String stanza = doc.get("stanza");
                 String content = doc.get("content");
-                String preview = content.length() > 200 ? content.substring(0, 200) + "..." : content;
-                System.out.println("Document preview: " + preview);
+                
+                System.out.println("\n----- Stanza " + stanza + " -----");
+                System.out.println("Section: " + section + " - " + sectionTitle);
+                System.out.println("\n" + content);
             }
             
             reader.close();
-            directory.close();
+            
         } catch (Exception e) {
             e.printStackTrace();
         }
